80e1e1020efa58b9600c40745c1aaa8b458ea3af
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-11-25 10:40:05 +0200

    fixing bugs fixing the params in request issue

--- the-experts-BE/routes/contractor-results.js
+++ the-experts-BE/routes/contractor-results.js
@@ -1,11 +1,11 @@
 var express = require("express");
-var router = express.Router();
+var router = express.Router({ mergeParams: true });
 var queries= require("../queries/index")
 router.get("/", function(req, res, next) {
-    console.log("hellow world")
+    console.log(req.params)
     queries.getContractor(req.params.job,(err,response)=>{
         if(err) console.log(err);
-
+// console.log(response);
         res.send(response);
 
     })
