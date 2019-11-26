ceff0229c1413cdb6940588a9d3bfb8c04468014
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-11-26 16:09:05 +0200

    depolied added the react static build folder closes #7

--- the-experts-BE/app.js
+++ the-experts-BE/app.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(path.join(__dirname, '..', 'the-experts-fe', 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'the-experts-fe', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname,'..', 'the-experts-fe', 'build', 'index.html'));
   });
 }
 
