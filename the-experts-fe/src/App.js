016b6b69a5bb5dba4360ea1eeaf94982c27c1f48
omriza5 <omriza5@gmail.com>
2019-11-26 12:27:01 +0200

    modify: contractors componet to render data close #31

--- the-experts-fe/src/App.js
+++ the-experts-fe/src/App.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Select from "./components/select/select";
 import Contractors from "./components/contractors/contractors";
-import Contractor from "./components/contractor/contractor";
 
 export default function App() {
   const [jobsArray, setJobArray] = useState([
