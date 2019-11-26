08a87679986a1f6b663976a332a94b82cd67d2e2
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-11-26 15:06:00 +0200

    try to deploy, just testing deploy

---
+++ the-experts-BE/the-experts-fe/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    // console.log(process.env.REACT_APP_testAPI2)
+    fetch("testApi")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">;{this.state.apiResponse}</p>
+        <p className="App-intro">;{"xxxx"}</p>
+
+      </div>
+    );
+  }
+}
+
+export default App;
