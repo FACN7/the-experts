0d4d5d10f5a4731641e49fd405efb12dc480e718
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-11-27 11:16:10 +0200

    cleanup from express app delete all the views delete the extra routes delete unnecessary lines in app.js and www file closes #30

--- the-experts-BE/app.js
+++ the-experts-BE/app.js
@@ -4,37 +4,34 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const testAPIRouter = require("./routes/testAPI");
 const getContractor = require("./routes/contractor-results");
 const cors = require("cors");
 
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 app.use(cors());
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/users", usersRouter);
-app.use("/testAPI", testAPIRouter);
 app.use("/contractor-results/:job", getContractor);
 
-if (process.env.NODE_ENV === 'production') {
-
-  app.use(express.static(path.join(__dirname, '..', 'the-experts-fe', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'..', 'the-experts-fe', 'build', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(path.join(__dirname, "..", "the-experts-fe", "build"))
+  );
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "..", "the-experts-fe", "build", "index.html")
+    );
   });
 }
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
