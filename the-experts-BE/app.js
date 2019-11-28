ededeffde8b8bee0018d8b73195ce43bd291f249
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-11-28 11:55:12 +0200

    connect the form to the BE delete the contractor-post file add the endpoint to app.js closes #16

--- the-experts-BE/app.js
+++ the-experts-BE/app.js
@@ -43,7 +43,6 @@ if (process.env.NODE_ENV === "production") {
 }
 // error handler
 app.use(function(err, req, res, next) {
-  console.log("ebr");
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
