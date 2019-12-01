48d305ebc4003e5398f57f2d66de1b108fa830c3
later009 <hosen.agbaria@hotmail.com>
2019-12-02 00:11:15 +0200

    resolved the small  issues

--- the-experts-fe/src/App.js
+++ the-experts-fe/src/App.js
@@ -2,21 +2,21 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import RegisterationForm from "./components/registerationForm/registerationForm";
-import ContractorProfile from "./components/ContractorProfile/ContractorProfile"
+import ContractorProfile from "./components/ContractorProfile/ContractorProfile";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/home/home";
-
 
 export default function App() {
   return (
     <React.Fragment>
       <NavBar firstParm="Login" secondParam="Register" />
-        <Switch className="App">
-          <Route path="/register" component={RegisterationForm}></Route>
-          <Route path="/ContractorProfile/:id"  component={ContractorProfile} />}/>
-          <Route exact path="/" component={Home}></Route>
-        </Switch>
-        <ReviewForm></ReviewForm>
+      <Switch className="App">
+        <Route path="/register" component={RegisterationForm}></Route>
+        <Route path="/ContractorProfile/:id" component={ContractorProfile} />
+        }/>
+        <Route exact path="/" component={Home}></Route>
+      </Switch>
+      <ReviewForm></ReviewForm>
     </React.Fragment>
   );
 }
--- the-experts-fe/src/components/ContractorProfile/ContractorProfile.jsx
+++ the-experts-fe/src/components/ContractorProfile/ContractorProfile.jsx
@@ -3,14 +3,10 @@ import Contractor from "../contractor/contractor";
 import "./ContractorProfile.css";
 
 export default function ContractorProfile(props) {
-  
   const contractor = props.location.state.contractor;
-  console.log(contractor);
-  
+  // jamie try to dismiss this please this is for future use (tomorrow-plan) please.. :)
   const ContractorProfile = () => {
-
-    fetch(`/getReview/${contractor.id}`)
-      .then(res => res.json());
+    fetch(`/getReview/${contractor.id}`).then(res => res.json());
   };
 
   return (
@@ -19,15 +15,15 @@ export default function ContractorProfile(props) {
         <img src="https://www.w3schools.com/bootstrap4/img_avatar3.png" />
       </div>
 
-      <h1 className="contractor-name"> { contractor.cont_name }</h1>
+      <h1 className="contractor-name"> {contractor.cont_name}</h1>
       <p className="contractor-description">{`${contractor.cont_name} is an experienced ${contractor.job} `}</p>
       <p className="contractor-rating">rating : {contractor.rating}</p>
       <p className="contractor-feedBacks">FeedBacks(23{}) </p>
 
       <button className="revealContact-btn"> reveal contact into </button>
 
-     {/* jamie i know that i should not keep any commented nonfunctional code but it's just a reminder  */}
-     {/* <Comment /> */}
+      {/* jamie i know that i should not keep any commented nonfunctional code but it's just a reminder  */}
+      {/* <Comment /> */}
     </div>
   );
 }
