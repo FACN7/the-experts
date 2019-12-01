bf7fbd95e2652a7a06faab30614e2b97599950b8
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-12-01 10:58:51 +0200

    create Reviews table adding fields (user_id, contractor_id, reviewBody, isliked) and adding new review as sample closes #55

--- the-experts-BE/database/db_build.sql
+++ the-experts-BE/database/db_build.sql
@@ -1,45 +1,56 @@
 BEGIN;
-    DROP TABLE IF EXISTS Contractor, Users;
 
-    CREATE TABLE
-    IF NOT EXISTS Contractor
-    (
-        id SERIAL PRIMARY KEY,
-        cont_name VARCHAR
-    (50) NOT NULL,
-        job TEXT NOT NULL,
-        rating double precision	 DEFAULT 0
-    );
+DROP TABLE IF EXISTS Contractor,
+Users;
 
-    CREATE TABLE
-    IF NOT EXISTS Users
-    (
-        id SERIAL PRIMARY KEY,
-        first_name VARCHAR
-    (50) NOT NULL,
-            last_name VARCHAR
-    (50) NOT NULL,
-        email TEXT NOT NULL,
-        user_password TEXT NOT NULL
-    );
+CREATE TABLE IF NOT EXISTS Contractor (
+    id SERIAL PRIMARY KEY,
+    cont_name VARCHAR (50) NOT NULL,
+    job TEXT NOT NULL,
+    rating double precision DEFAULT 0
+);
 
-INSERT INTO Users
-    (first_name,last_name,email,user_password)
+CREATE TABLE IF NOT EXISTS Users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    email TEXT NOT NULL,
+    user_password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Reviews (
+    id SERIAL,
+    user_id int NOT NULL,
+    contractor_id int NOT NULL,
+    reviewBody TEXT,
+    isliked BOOLEAN,
+    FOREIGN KEY (contractor_id) REFERENCES Contractor (id),
+    FOREIGN KEY (user_id) REFERENCES Users (id)
+);
+
+INSERT INTO
+    Users (first_name, last_name, email, user_password)
 VALUES
-    ('ebraheem', 'abbas', 'ebraheemabbas51@gmail.com', 'password');
+    (
+        'ebraheem',
+        'abbas',
+        'ebraheemabbas51@gmail.com',
+        'password'
+    );
 
-
-
-
-
-INSERT INTO Contractor
-    (cont_name,job,rating)
+INSERT INTO
+    Contractor (cont_name, job, rating)
 VALUES
     ('ali', 'plumber', 3.5);
 
-INSERT INTO Contractor
-    (cont_name,job,rating)
+INSERT INTO
+    Contractor (cont_name, job, rating)
 VALUES
     ('samih', 'plumber', 2);
+
+INSERT INTO
+    Reviews (user_id, contractor_id, reviewBody, isliked)
+VALUES
+    (1, 1, 'Ali is expert plumber', True);
 
 COMMIT;
