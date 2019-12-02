6d6b28f5b7027588428c51d67211adfcbccd428e
omriza5 <omriza5@gmail.com>
2019-12-02 11:14:44 +0200

    fix: import statement for register form close #74

--- the-experts-fe/src/App.js
+++ the-experts-fe/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import RegisterForm from "./components/RegisterForm/RegisterForm";
 import LoginForm from "./components/loginForm/loginForm";
+import RegisterForm from "./components/registerForm/registerForm";
 import ContractorProfile from "./components/ContractorProfile/ContractorProfile";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/home/home";
